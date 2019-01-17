1145e930308b84ac01dbcaa2d7efb7d58d325c04
Andreas Wittig <andreas@widdix.de>
2019-01-17 13:59:36 +0100

    added lambda layer module (#5)

--- test/test.js
+++ test/test.js
@@ -11,3 +11,32 @@ test('defaults', async t => {
     t.pass();
   }
 });
+
+test('lambda-layer', async t => {
+  const bucketStackName = cfntest.stackName();
+  const stackName = cfntest.stackName();
+  const contentKey = 'test.zip';
+  const dummyFilePath = `${__dirname}/dummy.zip`;
+  let bucketStackOutputs;
+
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/node_modules/@cfn-modules/s3-bucket/module.yml`, bucketStackName, {Versioning: 'false'}));
+    
+    bucketStackOutputs = await cfntest.getStackOutputs(bucketStackName);
+
+    await cfntest.createObject(bucketStackOutputs.Name, contentKey, dummyFilePath);
+    
+    t.log(await cfntest.createStack(`${__dirname}/layer.yml`, stackName, {
+      ContentBucket: bucketStackOutputs.Name,
+      ContentKey: contentKey
+    }));
+    // what could we test here?
+  } finally {
+    if (bucketStackOutputs) {
+      t.log(await cfntest.deleteObject(bucketStackOutputs.Name, contentKey));
+    }
+    t.log(await cfntest.deleteStack(stackName));
+    t.log(await cfntest.deleteStack(bucketStackName));
+    t.pass();
+  }
+});
